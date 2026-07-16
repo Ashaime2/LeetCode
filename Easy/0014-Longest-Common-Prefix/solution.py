a53c6d3a0f6c591b3d2f52d84370d307281530fe
Ashaime2 <130389948+Ashaime2@users.noreply.github.com>
2026-07-17 01:30:40 +0200

    🟢 Add 0014. Longest Common Prefix [Easy] (Python)

--- Easy/0014-Longest-Common-Prefix/solution.py
+++ Easy/0014-Longest-Common-Prefix/solution.py
@@ -1,10 +1,10 @@
 # ═══════════════════════════════════════════════════════
 #  Problem  : 0014. Longest Common Prefix
-#  URL      : https://leetcode.com/problems/longest-common-prefix/
+#  URL      : https://leetcode.com/problems/longest-common-prefix/submissions/2070454257/
 #  Difficulty : Easy
 #  Language : Python
 #  Runtime  : 0 ms
-#  Memory   : 12.4 MB
+#  Memory   : 12.5 MB
 #  Solved   : July 17, 2026
 # ═══════════════════════════════════════════════════════
 
@@ -18,11 +18,12 @@ class Solution(object):
         strs = sorted(strs, key=len)
 
         string = ''
-        for  i in range(n):
+        for  i in range(len(strs[0])):
             test = strs[0][i]
             for elt in strs :
                 if elt[i]!=test :
                     return string
             string += test
+        return string
 
 
