78f7d5b54c6bcddda5ca6fd73007dd873e271070
Ashaime2 <130389948+Ashaime2@users.noreply.github.com>
2026-07-18 00:42:06 +0200

    🟡 Add 0019. Remove Nth Node From End of List [Medium] (Python)

--- Medium/0019-Remove-Nth-Node-From-End-of-List/solution.py
+++ Medium/0019-Remove-Nth-Node-From-End-of-List/solution.py
@@ -3,37 +3,28 @@
 #  URL      : https://leetcode.com/problems/remove-nth-node-from-end-of-list/
 #  Difficulty : Medium
 #  Language : Python
-#  Runtime  : 0 ms
-#  Memory   : 12.6 MB
+#  Runtime  : 4 ms
+#  Memory   : 12.4 MB
 #  Solved   : July 18, 2026
 # ═══════════════════════════════════════════════════════
 
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution(object):
     def removeNthFromEnd(self, head, n):
-        """
-        :type head: Optional[ListNode]
-        :type n: int
-        :rtype: Optional[ListNode]
-        """
-        if head.next == None :
-            return head
         node = head
-        compteur = 0
-        while node != None :
-            compteur += 1
-            node = node.next
-        node = head
-        for i in range(compteur-n-1):
-            node = node.next
-        bon = node
-        effacer = node.next
-        raccord = effacer.next
-        bon.next = raccord
-        return head
+        length = 0
 
-        
+        while node:
+            length += 1
+            node = node.next
+
+        # Remove the first node
+        if n == length:
+            return head.next
+
+        node = head
+        for _ in range(length - n - 1):
+            node = node.next
+
+        node.next = node.next.next
+
+        return head
