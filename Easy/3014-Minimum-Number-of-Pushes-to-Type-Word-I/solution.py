7a332ea1d085ef4fa177f4838c53bb48f249d5bd
Ashaime2 <130389948+Ashaime2@users.noreply.github.com>
2026-07-31 00:35:35 +0200

    🟢 Add 3014. Minimum Number of Pushes to Type Word I [Easy] (Python)

---
+++ Easy/3014-Minimum-Number-of-Pushes-to-Type-Word-I/solution.py
@@ -0,0 +1,24 @@
+# ═══════════════════════════════════════════════════════
+#  Problem  : 3014. Minimum Number of Pushes to Type Word I
+#  URL      : https://leetcode.com/problems/minimum-number-of-pushes-to-type-word-i/submissions/2088084166/?envType=daily-question&envId=2026-07-30
+#  Difficulty : Easy
+#  Language : Python
+#  Runtime  : 0 ms
+#  Memory   : 12.3 MB
+#  Solved   : July 31, 2026
+# ═══════════════════════════════════════════════════════
+
+class Solution(object):
+    def minimumPushes(self, word):
+        n = len(word)
+
+        if n <= 8:
+            return n
+
+        if n <= 16:
+            return 8 + (n - 8) * 2
+
+        if n <= 24:
+            return 8 + 16 + (n - 16) * 3
+
+        return 8 + 16 + 24 + (n - 24) * 4
